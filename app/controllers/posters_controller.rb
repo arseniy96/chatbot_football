8cb17f25e6bfdfa5e31665b4675ba2ac6b073280
arseniy96 <arsenzhar@gmail.com>
2018-06-07 16:54:11 +0300

    Add PostsController

---
+++ app/controllers/posters_controller.rb
@@ -0,0 +1,29 @@
+class PostersController < ApplicationController
+
+  def index
+
+  end
+
+  def show
+    @poster = Poster.find(params[:id])
+  end
+
+  def create
+    username = params[:poster][:username]
+    user_id = Poster.count
+    img = Magick::Image.read(Rails.root + 'public/test.jpg').first
+
+    copyright = Magick::Draw.new
+    copyright.annotate(img, 0, 0, 100, 280, username) do
+      self.font = 'Georgia'
+      self.pointsize = 24
+      self.font_weight = Magick::BoldWeight
+      self.fill = 'white'
+      self.gravity = Magick::SouthEastGravity
+    end
+    img.write(Rails.root + "public/test#{user_id}.jpg")
+    @poster = Poster.create(user_id: user_id, username: username)
+    redirect_to @poster
+  end
+
+end
