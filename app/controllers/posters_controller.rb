f3bcf1a9ac35410d8d5a74d908b099f9b29d00d7
arseniy96 <arsenzhar@gmail.com>
2018-06-07 18:16:51 +0300

    Add Carrierwave

--- app/controllers/posters_controller.rb
+++ app/controllers/posters_controller.rb
@@ -22,7 +22,8 @@ class PostersController < ApplicationController
       self.gravity = Magick::SouthEastGravity
     end
     img.write(Rails.root + "public/test#{user_id}.jpg")
-    @poster = Poster.create(user_id: user_id, username: username)
+    poster_image = File.open(Rails.root + "public/test#{user_id}.jpg")
+    @poster = Poster.create(user_id: user_id, username: username, image: poster_image)
     redirect_to @poster
   end
 
