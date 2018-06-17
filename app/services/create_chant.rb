fbcbaef635d17e1bdeb3c3407e025ad96ba74070
arseniy96 <arsenzhar@gmail.com>
2018-06-17 14:08:04 +0300

    Add service CreateChant

---
+++ app/services/create_chant.rb
@@ -0,0 +1,22 @@
+module Services
+  class CreateChant
+
+    def self.call(user_id)
+      user = CreateUser.call(user_id)
+      img = Magick::Image.read(Rails.root + 'public/test.jpg').first
+      copyright = Magick::Draw.new
+      copyright.annotate(img, 0, 0, 100, 280, user.firstname) do
+        self.font = 'Georgia'
+        self.pointsize = 24
+        self.font_weight = Magick::BoldWeight
+        self.fill = 'white'
+        self.gravity = Magick::SouthEastGravity
+      end
+      img_name = Time.now.to_i.to_s + user_id.to_s
+      img.write(Rails.root + "public/#{img_name}.jpg")
+      chant_image = File.open(Rails.root + "public/#{img_name}.jpg")
+      user.chants.create(text: 'Blah', image: chant_image)
+    end
+
+  end
+end
