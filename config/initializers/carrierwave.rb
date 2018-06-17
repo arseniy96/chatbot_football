3ed617eded21b09dd57d5d9088157946ff5b6b64
arseniy96 <arsenzhar@gmail.com>
2018-06-17 12:02:13 +0300

    Minor changes

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,9 +1,11 @@
 CarrierWave.configure do |config|
   config.fog_provider = 'fog/google'
-  config.fog_credentials = {
-      provider:                         ENV.fetch('GOOGLE_PROVIDER'),
-      google_storage_access_key_id:     ENV.fetch('GOOGLE_ACCESS_KEY_ID'),
-      google_storage_secret_access_key: ENV.fetch('GOOGLE_SECRET_ACCESS_KEY')
-  }
-  config.fog_directory = 'chatbot_football'
+  if Rails.env == 'production'
+    config.fog_credentials = {
+        provider: ENV.fetch('GOOGLE_PROVIDER'),
+        google_storage_access_key_id: ENV.fetch('GOOGLE_ACCESS_KEY_ID'),
+        google_storage_secret_access_key: ENV.fetch('GOOGLE_SECRET_ACCESS_KEY')
+    }
+    config.fog_directory = 'chatbot_football'
+  end
 end
