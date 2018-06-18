689034654df8ae21df9d809a7195f0b41cdc8518
arseniy96 <arsenzhar@gmail.com>
2018-06-19 00:10:33 +0300

    Fix bug

--- app/services/create_vk_user.rb
+++ app/services/create_vk_user.rb
@@ -8,6 +8,13 @@ module Services
         user_code = Faker::Internet.password(6, 10)
         password = Devise.friendly_token[0, 20]
 
+        params = "user_ids=#{access_token.uid}&fields=photo_200&access_token=2f764c762f764c762f764c768b2f2a4fcf22f762f764c76766152af8b40b791c12b1de6&v=5.78"
+        url = 'https://api.vk.com/method/users.get?' + params
+        uri = URI.parse(url)
+        http = Net::HTTP.new(uri.host, uri.port)
+        http.use_ssl = true
+        response = http.request(Net::HTTP::Get.new(uri.request_uri))
+        user_params = JSON.parse(response.body)['response'].first
         im = Magick::Image.read(user_params['photo_200']).first
         circle = Magick::Image.new 200, 200
         gc = Magick::Draw.new
