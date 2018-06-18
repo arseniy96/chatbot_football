7adee6e6134da59d733b3f2d06726394b7b44751
arseniy96 <arsenzhar@gmail.com>
2018-06-18 15:09:42 +0300

    Add Devise for User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable,
+         :omniauthable, omniauth_providers: [:vkontakte]
   has_many :chants
 
   mount_uploader :avatar, PosterUploader
