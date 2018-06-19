242427caae4c102694a571d0b07a096e1b7d8425
arseniy96 <arsenzhar@gmail.com>
2018-06-19 12:13:08 +0300

    Minor changes

--- app/services/create_vk_user.rb
+++ app/services/create_vk_user.rb
@@ -8,14 +8,15 @@ module Services
         user_code = Faker::Internet.password(6, 10)
         password = Devise.friendly_token[0, 20]
 
-        params = "user_ids=#{access_token.uid}&fields=photo_200&access_token=2f764c762f764c762f764c768b2f2a4fcf22f762f764c76766152af8b40b791c12b1de6&v=5.78"
-        url = 'https://api.vk.com/method/users.get?' + params
-        uri = URI.parse(url)
-        http = Net::HTTP.new(uri.host, uri.port)
-        http.use_ssl = true
-        response = http.request(Net::HTTP::Get.new(uri.request_uri))
-        user_params = JSON.parse(response.body)['response'].first
-        im = Magick::Image.read(user_params['photo_200']).first
+        # params = "user_ids=#{access_token.uid}&fields=photo_200&access_token=2f764c762f764c762f764c768b2f2a4fcf22f762f764c76766152af8b40b791c12b1de6&v=5.78"
+        # url = 'https://api.vk.com/method/users.get?' + params
+        # uri = URI.parse(url)
+        # http = Net::HTTP.new(uri.host, uri.port)
+        # http.use_ssl = true
+        # response = http.request(Net::HTTP::Get.new(uri.request_uri))
+        # user_params = JSON.parse(response.body)['response'].first
+        # im = Magick::Image.read(user_params['photo_200']).first
+        im = Magick::Image.read(access_token.info.image).first
         circle = Magick::Image.new 200, 200
         gc = Magick::Draw.new
         gc.fill 'black'
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -9,7 +9,7 @@ Devise.setup do |config|
   # Devise will use the `secret_key_base` as its `secret_key`
   # by default. You can change it below and use your own secret key.
   # config.secret_key = '684feaf5bb12a0af07e97e4e071ffa379511d550d781488703d9ed612fca8196c2dde2bc50dca34578b80ba6930e8874de32806dd8d7e52fe0210c4885d816da'
-  
+
   # ==> Controller configuration
   # Configure the parent class to the devise controllers.
   # config.parent_controller = 'DeviseController'
@@ -257,7 +257,17 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
-  config.omniauth :vkontakte, ENV['VK_APP_ID'], ENV['VK_APP_SECRET'], lang: 'ru', https: 1 if Rails.env == 'production'
+  config.omniauth :vkontakte, ENV['VK_APP_ID'], ENV['VK_APP_SECRET'],
+                  {
+                      scope: 'friends,photos,groups',
+                      display: 'popup',
+                      lang: 'en',
+                      https: 1,
+                      image_size: 'bigger_x2',
+                      redirect_url: 'https://football-chatbot.herokuapp.com/users/auth/vkontakte/callback',
+                      v: '5.80',
+                      callback_path: '/users/auth/vkontakte/callback'
+                  }
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
