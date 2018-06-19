2b34c8f47a9b6fa31480282058ca09b33f611450
arseniy96 <arsenzhar@gmail.com>
2018-06-19 17:03:54 +0300

    Comment bag

--- app/services/create_user.rb
+++ app/services/create_user.rb
@@ -19,25 +19,25 @@ module Services
         response = http.request(Net::HTTP::Get.new(uri.request_uri))
         user_params = JSON.parse(response.body)['response'].first
 
-        im = Magick::Image.read(user_params['photo_200']).first
-        circle = Magick::Image.new 200, 200
-        gc = Magick::Draw.new
-        gc.fill 'black'
-        gc.circle 100, 100, 100, 1
-        gc.draw circle
-        mask = circle.blur_image(0, 1).negate
-        mask.matte = false
-        im.matte = true
-        im.composite!(mask, Magick::CenterGravity, Magick::CopyOpacityCompositeOp)
-        img_name = 'avatar' + user_id.to_s
-        im.write(Rails.root + "public/#{img_name}.png")
-        avatar_img = File.open(Rails.root + "public/#{img_name}.png")
+        # im = Magick::Image.read(user_params['photo_200']).first
+        # circle = Magick::Image.new 200, 200
+        # gc = Magick::Draw.new
+        # gc.fill 'black'
+        # gc.circle 100, 100, 100, 1
+        # gc.draw circle
+        # mask = circle.blur_image(0, 1).negate
+        # mask.matte = false
+        # im.matte = true
+        # im.composite!(mask, Magick::CenterGravity, Magick::CopyOpacityCompositeOp)
+        # img_name = 'avatar' + user_id.to_s
+        # im.write(Rails.root + "public/#{img_name}.png")
+        # avatar_img = File.open(Rails.root + "public/#{img_name}.png")
 
         User.create(uid: user_id.to_i,
                     provider: 'vkontakte',
                     firstname: user_params['first_name'],
                     lastname: user_params['last_name'],
-                    avatar: avatar_img,
+                    # avatar: avatar_img,
                     code: user_code,
                     email: user_id.to_s + user_code + '@vk.com',
                     password: password,
