a4f0284adf6efb5b602fda7f7cdd9e71066818d3
arseniy96 <arsenzhar@gmail.com>
2018-06-20 11:12:46 +0300

    Fix bug

--- app/controllers/chants_controller.rb
+++ app/controllers/chants_controller.rb
@@ -1,5 +1,5 @@
 class ChantsController < ApplicationController
-  
+
   before_action :authenticate_admin!, only: [:index]
 
   def index
@@ -14,7 +14,7 @@ class ChantsController < ApplicationController
   def create
     if user_signed_in?
       @chant = Services::CreateChant.call(current_user)
-      redirect_to @chant
+      redirect_to user_chant_path(current_user, @chant)
     else
       redirect_to 'https://oauth.vk.com/authorize?client_id=6609013&display=popup&redirect_uri=https%3A%2F%2Ffootball-chatbot.herokuapp.com%2Fcallback&response_type=code&scope=friends%2Cphotos%2Cgroups&v=5.80'
     end
