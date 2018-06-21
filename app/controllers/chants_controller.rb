a21faa38a7eeb135ed4e24e54d6ccf84081e7209
arseniy96 <arsenzhar@gmail.com>
2018-06-21 18:31:31 +0300

    Add chants#new

--- app/controllers/chants_controller.rb
+++ app/controllers/chants_controller.rb
@@ -1,6 +1,7 @@
 class ChantsController < ApplicationController
 
   before_action :authenticate_admin!, only: [:index]
+  before_action :authenticate_user!, only: [:new, :create]
 
   def index
 
@@ -11,9 +12,14 @@ class ChantsController < ApplicationController
     @user = User.find(params[:user_id])
   end
 
+  def new
+    @chant = Chant.new
+  end
+
   def create
     if user_signed_in?
-      @chant = Services::CreateChant.call(current_user)
+      chant_params = params.require(:chant).permit!.to_h
+      @chant = Services::CreateChant.call(current_user, chant_params)
       redirect_to user_chant_path(current_user, @chant)
     else
       redirect_to 'https://oauth.vk.com/authorize?client_id=6609013&display=popup&redirect_uri=https%3A%2F%2Ffootball-chatbot.herokuapp.com%2Fcallback&response_type=code&scope=friends%2Cphotos%2Cgroups&v=5.80'
--- app/services/create_chant.rb
+++ app/services/create_chant.rb
@@ -1,7 +1,7 @@
 module Services
   class CreateChant
 
-    def self.call(user)
+    def self.call(user, chant_params)
       img = Magick::Image.read(Rails.root + 'public/test.jpg').first
       copyright = Magick::Draw.new
       copyright.annotate(img, 0, 0, 100, 280, user.firstname) do
@@ -14,7 +14,7 @@ module Services
       img_name = (Time.now.to_i + user.id.to_i).to_s
       img.write(Rails.root + "public/#{img_name}.jpg")
       chant_image = File.open(Rails.root + "public/#{img_name}.jpg")
-      user.chants.create(text: 'Blah', image: chant_image)
+      user.chants.create(text: 'Blah', image: chant_image, country1: chant_params[:country1], country2: chant_params[:country2])
     end
 
   end
