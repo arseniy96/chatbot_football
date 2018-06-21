834f3a824ded41598e3d6765a7e0b921755c6bc3
arseniy96 <arsenzhar@gmail.com>
2018-06-21 20:07:26 +0300

    Add :rating to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180621154304) do
+ActiveRecord::Schema.define(version: 20180621161450) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -78,6 +78,7 @@ ActiveRecord::Schema.define(version: 20180621154304) do
     t.text "groups"
     t.text "posts"
     t.string "invite_code"
+    t.integer "rating"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
