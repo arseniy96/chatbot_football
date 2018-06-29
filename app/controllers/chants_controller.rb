9fbfa51d8080cb74551227f5c8820551f9f8a902
arseniy96 <arsenzhar@gmail.com>
2018-06-29 18:03:37 +0300

    Update chants#show page

--- app/controllers/chants_controller.rb
+++ app/controllers/chants_controller.rb
@@ -8,6 +8,7 @@ class ChantsController < ApplicationController
   end
 
   def show
+    @user = User.find(params[:user_id])
     @chant = Chant.find(params[:id])
     @user = User.find(params[:user_id])
   end
